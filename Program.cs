6061be7d2645fd5ee6df0e2896db11c9d74cd210
stephensenft21 <stephensenft21@gmail.com>
2019-11-15 11:22:49 -0600

    Intro to C# making classes

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Adam";
+            int cohort = 34;
+
+            // reassigning varaibles
+            name = "Jisie";
+            cohort = 35;
+            //string concatenation
+
+            string greeting = "Hello " + name + ". Welcome to cohort " + cohort;
+            // string interpolation 
+            string greeting2 = $"Hello {name}. Welcome to Cohort {cohort}";
+
+            bool isTheBest = cohort == 35;
+            if (isTheBest)
+            {
+                Console.WriteLine("Ah, the best around");
+            }
+            else
+            {
+                Console.WriteLine("You ");
+            }
+
+            Console.WriteLine(greeting2);
+
+            int[] cohorts = new int[]
+            {
+                34,
+                35,
+                36,
+                37
+            };
+            for (int i = 0; i < cohorts.Length; i++)
+            {
+                Console.WriteLine($"Cohort {cohorts[i]}");
+            }
+            foreach (int taco in cohorts)
+            {
+                Console.WriteLine($"{taco}");
+            }
+
+            while (cohort > 0)
+            {
+                cohort--;
+                Console.WriteLine(cohort);
+            }
+
+        }
+    }
+
+}
